0ed224a844bb7f58c140687eadb053bd6e60e072
Lucas Cupertino <lucas.cupertino.usp.br>
2020-02-14 18:09:24 -0300

    Update

--- func/UnetModel.py
+++ func/UnetModel.py
@@ -20,6 +20,7 @@ import numpy as np
 from numpy import random
 from keras import layers
 from keras import models
+from keras import optimizers
 
 ## Kernel size = 3*3
 ## Stride = 1 // Padding = 1
@@ -38,19 +39,19 @@ print('DNN implementation using Keras')
 class unetConv2(model):
     def __init__(self, in_size, out_size, is_batchnorm):
         super(unetConv2, self).__init__()
-        # Original sequence of operation if is_batchnorm = True
+        # Original sequence of operation if is_batchnorm = True:
         # Conv2D, BN, ReLU
         if is_batchnorm:
-            self.conv1 = model.add(layers.Conv2D(in_size, out_size, 3, 1, 1, activation = 'relu'),
+            self.conv1 = model.add(layers.Conv2D(in_size,''' out_size,''' kernel_size = (3, 3), strides = (1, 1), padding = 1, activation = 'relu'),
                                    layers.BatchNormalization(out_size))
             
-            self.conv2 = model.add(layers.Conv2D(out_size, out_size, 3, 1, 1, activation = 'relu'),
+            self.conv2 = model.add(layers.Conv2D(out_size, '''out_size,''' kernel_size = (3, 3), strides = (1, 1), padding = 1, activation = 'relu'),
                                    layers.BatchNormalization(out_size))
 
         else:
-            self.conv1 = model.add(layers.Conv2d(in_size, out_size, 3, 1, 1, activation = 'relu'))
+            self.conv1 = model.add(layers.Conv2d(in_size, '''out_size,''' kernel_size = (3, 3) strides = (1, 1), padding  = 1, activation = 'relu'))
 
-            self.conv2 = model.add(layers.Conv2d(out_size, out_size, 3, 1, 1, activation = 'relu'))
+            self.conv2 = model.add(layers.Conv2d(out_size, '''out_size,''' kernel_size = (3, 3), strides = (1, 1), padding = 1, activation = 'relu'))
 
                                        
     def forward(self, inputs):
@@ -65,7 +66,7 @@ class unetDown(model):
         super(unetDown, self).__init__()
         self.conv = unetConv2(in_size, out_size, is_batchnorm)
         ##self.down = nn.MaxPool2d(2, 2, ceil_mode=True)
-        self.down = model.add(layers.MaxPooling2D(pool_size = (2,2), strides = 1))
+        self.down = model.add(layers.MaxPooling2D(pool_size = (2,2)''', strides = 1'''))
 
     def forward(self, inputs):
         outputs = self.conv(inputs)
@@ -80,9 +81,9 @@ class unetUp(model):
         self.conv = unetConv2(in_size, out_size, True)
         # Transposed convolution
         if is_deconv:
-            self.up = model.add(layers.Conv2DTranspose(in_size, kernel_size=2,stride=2))
+            self.up = model.add(layers.Conv2DTranspose(in_size, kernel_size= (2, 2), stride = 2))
         else:
-            self.up = model.add(layers.Upsampling2D(size = (2, 2), interpolation = 'bilinear'))
+            self.up = model.add(layers.Upsampling2D(kernel_size = (2, 2), interpolation = 'bilinear'))
 
     def forward(self, inputs1, inputs2):
         outputs2 = self.up(inputs2)
@@ -101,9 +102,9 @@ class UnetModel(model):
         self.is_deconv     = is_deconv
         self.in_channels   = in_channels
         self.is_batchnorm  = is_batchnorm
-        self.n_classes     =n_classes
+        self.n_classes     = n_classes
         
-        filters = [64, 128, 256, 512, 1024]
+        filters = [64, 128, 256, 512, 1024, 2048]
         
         self.down1   = unetDown(self.in_channels, filters[0], self.is_batchnorm)
         self.down2   = unetDown(filters[0], filters[1], self.is_batchnorm)
@@ -114,7 +115,7 @@ class UnetModel(model):
         self.up3     = unetUp(filters[3], filters[2], self.is_deconv)
         self.up2     = unetUp(filters[2], filters[1], self.is_deconv)
         self.up1     = unetUp(filters[1], filters[0], self.is_deconv)
-        self.final   = model.add(layers.Conv2d(filters[0],self.n_classes, 1))
+        self.final   = model.add(layers.Conv2D(filters[0],self.n_classes, 1))
         
     def forward(self, inputs,label_dsp_dim):
         down1  = self.down1(inputs)
@@ -133,15 +134,15 @@ class UnetModel(model):
     # Initialization of Parameters
     def  _initialize_weights(self):
           for m in self.modules():
-            if isinstance(m, keras.layers.Conv2D):
+            if isinstance(m, keras.layers.Conv2D()):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, sqrt(2. / n))
                 if m.bias is not None:
                     m.bias.data.zero_()
-            elif isinstance(m, keras.layers.BatchNormalization):
+            elif isinstance(m, keras.layers.BatchNormalization()):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
-            elif isinstance(m, keras.layers.Conv2DTranspose):
+            elif isinstance(m, keras.layers.Conv2DTranspose()):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, sqrt(2. / n))
                 if m.bias is not None:
